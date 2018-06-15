830faf5539b734e8d5dd45a97855ca472c56c80e
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-06-15 17:53:49 +0200

    Add minimal comments

--- src/main/java/StackTraceParser.java
+++ src/main/java/StackTraceParser.java
@@ -2,10 +2,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StackTraceParser {
-    // ^\tat ((?:(?:[\d\w]*\.)*[\d\w]*))\.([\d\w\$]*)\(([\d\w]*\.java):(\d*)\)$
-    // ^\tat ((?:(?:[\d\w]*\.)*[\d\w]*)(?:\.([\d\w\$]*)){1,2})\(([\d\w]*\.java):(\d*)\)$
-    // ^\tat ((?:(?:[\d\w]*\.)*[\d\w]*))\.([\d\w\$]*)\.([\d\w\$]*)\(([\d\w]*\.java):(\d*)\)$
-    // ^\tat ((?:(?:[\d\w]*\.)*[\d\w]*))\.([\d\w\$]*)\.([\d\w\$]*)\((?:(?:([\d\w]*\.java):(\d*))|(?:([\d\w]*) ([\d\w]*)))\)$
     // ^\tat ((?:(?:[\d\w]*\.)*[\d\w]*))\.([\d\w\$]*)\.([\d\w\$]*)\((?:(?:([\d\w]*\.java):(\d*))|([\d\w\s]*))\)$
     // group 1: package name
     // group 2: class name
@@ -13,8 +9,7 @@ public class StackTraceParser {
     // group 4: file name | null
     // group 5: line number | null
     // group 6: null | string
-    private static String STACK_LINE_REGEX =
-            "^\\tat ((?:(?:[\\d\\w]*\\.)*[\\d\\w]*))\\.([\\d\\w\\$]*)\\.([\\d\\w\\$]*)\\((?:(?:([\\d\\w]*\\.java):(\\d*))|([\\d\\w\\s]*))\\)$$";
+    private static String STACK_LINE_REGEX = "^\\tat ((?:(?:[\\d\\w]*\\.)*[\\d\\w]*))\\.([\\d\\w\\$]*)\\.([\\d\\w\\$]*)\\((?:(?:([\\d\\w]*\\.java):(\\d*))|([\\d\\w\\s]*))\\)$";
     private static Pattern STACK_LINE_PATTERN = Pattern.compile(STACK_LINE_REGEX);
 
     public static StackTrace parse(String stackTraceString) {
@@ -35,12 +30,14 @@ public class StackTraceParser {
                 if (matcher.group(4) != null) {
                     fileName = matcher.group(4);
                 }
-
+                
                 int lineNumber = -1;
                 if (matcher.group(5) != null) {
                     lineNumber = Integer.valueOf(matcher.group(5));
                 }
 
+                // pass -2 as line number if the method is a native method
+                // see isNativeMethod() of StackTraceElement implementation
                 if (matcher.group(6) != null && matcher.group(6).equals("Native Method")) {
                     lineNumber = -2;
 
